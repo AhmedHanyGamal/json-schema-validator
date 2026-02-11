03336822e3c8a039e34811bf10982b812f3a3fec
Ahmed Hany Gamal <a7madhany2003@gmail.com>
2026-02-11 19:38:39 +0200

    finished the basic output type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,6 @@
 import { ValidationContext } from "./validator.js";
+import { JSONPointer, AbsoluteJSONPointer } from "./utils/JSONPointer.js";
+
 
 export interface KeywordHandler {
     (schema: unknown, instance: JSONValue, context: ValidationContext): void;
@@ -7,6 +9,33 @@ export interface KeywordHandler {
 export interface KeywordRegistry {
     [key: string]: KeywordHandler;
 }
+
+
+
+interface basicBaseUnit {
+    valid: boolean;
+    evaluationPath: JSONPointer; // May change to AbsoluteJSONPointer, will see once I start working on `$ref`, `$dynamicRef` and similar keywords
+    schemaLocation: AbsoluteJSONPointer; 
+    instanceLocation: JSONPointer; 
+}
+
+export interface basicSuccessUnit extends basicBaseUnit {
+    valid: true;
+    annotations: Record<string, any>;
+}
+
+export interface basicFailUnit extends basicBaseUnit {
+    valid: false;
+    errors: Record<string, string>;
+}
+
+export type outputUnit = basicSuccessUnit | basicFailUnit
+
+export type basicOutput = {
+    valid: boolean;
+    details: outputUnit[];
+}
+
 
 
 export type JSONValue = null | boolean | number | string | JSONObject | JSONArray;
