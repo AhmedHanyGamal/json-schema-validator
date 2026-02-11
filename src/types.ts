702c3013cbebc08cea4860448822f7f7933a0f9b
Ahmed Hany Gamal <a7madhany2003@gmail.com>
2026-02-11 19:36:15 +0200

    updated the 'Schema' type

--- src/types.ts
+++ src/types.ts
@@ -1,27 +1,11 @@
 import { ValidationContext } from "./validator.js";
-// export type DraftKeywords = Record<string, Function>;
-
-// export type DraftKeywordsContainer = Record<string, DraftKeywords>;
-
-
-export interface Schema {
-    // (TASK)
-
-    [key: string]: unknown;
-};
 
 export interface KeywordHandler {
-    // (schema: Schema, instance: JSONValue, context: ValidationContext): void;
     (schema: unknown, instance: JSONValue, context: ValidationContext): void;
 }
 
 export interface KeywordRegistry {
     [key: string]: KeywordHandler;
-}
-
-export interface ValidationError {
-    valid: boolean;
-    // (TASK)
 }
 
 
@@ -33,7 +17,4 @@ export type JSONObject = {
     [key: string]: JSONValue
 }
 
-export interface DetailedOutput {
-    valid: boolean;
-    // (TASK)
-};
+export type Schema = JSONObject | boolean
