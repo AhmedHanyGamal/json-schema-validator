ae3c82dad9ed0134706aa14e263cc825bf4d1412
Ahmed Hany Gamal <a7madhany2003@gmail.com>
2026-02-23 16:13:52 +0200

    add BasicPendingUnit interface for output units that are being processed

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,7 @@ import { JSONPointer, AbsoluteJSONPointer } from "./utils/JSONPointer.js";
 
 
 export interface KeywordHandler {
-    (schema: JSONValue, instance: JSONValue, context: ValidationContext): void;
+    (schema: any, instance: JSONValue, context: ValidationContext, pendingUnit: BasicPendingUnit): boolean;
 }
 
 export interface KeywordRegistry {
@@ -12,8 +12,7 @@ export interface KeywordRegistry {
 
 
 
-interface BasicBaseUnit {
-    valid: boolean;
+export interface BasicBaseUnit {
     evaluationPath: JSONPointer; // May change to AbsoluteJSONPointer, will see once I start working on `$ref`, `$dynamicRef` and similar keywords
     schemaLocation: AbsoluteJSONPointer; 
     instanceLocation: JSONPointer; 
@@ -26,6 +25,11 @@ export interface BasicSuccessUnit extends BasicBaseUnit {
 
 export interface BasicFailUnit extends BasicBaseUnit {
     valid: false;
+    errors: Record<string, string>;
+}
+
+export interface BasicPendingUnit extends BasicBaseUnit {
+    annotations: Record<string, any>;
     errors: Record<string, string>;
 }
 
